a5ded4b72e65aa570047ca0c90fe37ecb86e65f1
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-11-03 00:09:00 +0200

    handeled login and logout functions, added kernel.php manually, not that midlleware is not working correctly --so i commnted it out untill i find solution in near future

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,18 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Auth;
-use Symfony\Component\HttpFoundation\Response;
+//use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class Admin
-{
+{ 
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+
         if(!Auth::guard('admin')->check()){
             return redirect()->route('login_from')->with('error','Plz Login First');
         }
